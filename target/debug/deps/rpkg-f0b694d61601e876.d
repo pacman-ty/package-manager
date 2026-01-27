fc75963e4942e7278cec9e3cdcccb20889572320
Talha Yildirim <talha.yildirim720@gmail.com>
2026-01-26 23:34:43 -0500

    bruh work

---
+++ target/debug/deps/rpkg-f0b694d61601e876.d
@@ -0,0 +1,6 @@
+/home/ty/projects/package-manager/target/debug/deps/rpkg-f0b694d61601e876.d: src/lib.rs src/debversion.rs
+
+/home/ty/projects/package-manager/target/debug/deps/librpkg-f0b694d61601e876.rmeta: src/lib.rs src/debversion.rs
+
+src/lib.rs:
+src/debversion.rs:
